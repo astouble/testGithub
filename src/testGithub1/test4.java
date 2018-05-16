e77303a541b1e681b093d0f6b9a1f6b7b0b073c3
libaoxin <baoxin.li@youyi-tech.com>
2018-05-16 13:06:42 +0800

    123

--- src/testGithub1/test4.java
+++ src/testGithub1/test4.java
@@ -1,8 +1,17 @@
 package testGithub1;
 
+import java.util.HashMap;
+
 public class test4 {
 
 	public static void main(String[] args) {
 		System.out.println("第四fetch");
+		Double ll = 8.16;
+		double ss = 13.6;
+		System.out.println((ll/ss)*100);
+		
+		HashMap<String,Double> monthPayMap=new HashMap<String,Double>();
+		monthPayMap.get("123");
+		System.out.println(monthPayMap.get("123"));
 	}
 }
