fc65a12afdaac3d2ba871d0eba540bce51f0391f
libaoxin <baoxin.li@youyi-tech.com>
2018-05-21 11:06:55 +0800

    冲突3

--- src/testGithub1/test4.java
+++ src/testGithub1/test4.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class test4 {
 
 	public static void main(String[] args) {
-		Double lls= 8.16;
+		System.out.println(monthPayMap.get("123"));
 		Double ll = 8.16;
 		double ss = 13.6;
 		System.out.println((ll/ss)*100);
