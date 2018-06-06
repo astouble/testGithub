3f3f7dccaa0b284731a887fa2d67b1466f692604
libaoxin <baoxin.li@youyi-tech.com>
2018-06-06 17:32:55 +0800

    三生三世

--- src/testGithub1/zxcxzz.java
+++ src/testGithub1/zxcxzz.java
@@ -1,10 +1,12 @@
 package testGithub1;
 
-import java.text.ParseException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,21 +28,85 @@ public class zxcxzz {
 		zongList.add(map1);
 		zongList.add(map2);
 		zongList.add(map3);
-		Collections.sort(zongList, new Comparator<Map>() {
-			@Override
-			public int compare(Map o1, Map o2) {
-				int ret = 0;
+//		Collections.sort(zongList, new Comparator<Map>() {
+//			@Override
+//			public int compare(Map o1, Map o2) {
+//				int ret = 0;
+//				try {
+//					// 比较两个对象的顺序，如果前者小于、等于或者大于后者，则分别返回-1/0/1
+//					ret = sdf.parse(o1.get("date").toString()).compareTo(sdf.parse(o2.get("date").toString()));
+//				} catch (ParseException e) {
+//					e.printStackTrace();
+//				}
+//				return ret;
+//			}
+//		});
+//		String;
+//		Map<String, Object>[] maap = new Map[5]; 
+//		Arrays.sort(zongList.toArray(new Map[0]));;
+//		for(Map<String, Object> mm:zongList) {
+//			System.out.println(mm.get("date"));
+//		}
+//		
+//		
+//		
+//		
+//		ArrayList<String> list=new ArrayList<String>();
+//		list.add("1");
+//		list.add("2");
+//		list.add("4");
+//		list.add("0");
+//
+//		String[] strings =list.toArray(new String[0]);
+//		for(String s:strings) {
+//			System.out.println(s);
+//		}
+		
+		String ss="\t123\t按客户发动机\ta拉很多事\t打两局a\tad";
+
+		System.out.println("\t123\t按客户发动机\ta拉很多事\t打两局a\tad");
+		
+		BufferedWriter osw = null;
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream("F:\\ceshi.txt");
+			osw = new BufferedWriter(new OutputStreamWriter(fos, "UTF-8"));
+			osw.write(ss);
+			osw.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (osw != null) {
 				try {
-					// 比较两个对象的顺序，如果前者小于、等于或者大于后者，则分别返回-1/0/1
-					ret = sdf.parse(o2.get("date").toString()).compareTo(sdf.parse(o1.get("date").toString()));
-				} catch (ParseException e) {
-					e.printStackTrace();
+					osw.close();
+				} catch (Exception e1) {
+					e1.printStackTrace();
 				}
-				return ret;
 			}
-		});
-		for(Map<String, Object> mm:zongList) {
-			System.out.println(mm.get("date"));
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		
+		BufferedReader bre = null;
+		try {
+			String str="";
+		String file = "D:\\微博导出数据文件.txt";
+		bre = new BufferedReader(new FileReader(file));//此时获取到的bre就是整个文件的缓存流
+		while ((str = bre.readLine())!= null) // 判断最后一行不存在，为空结束循环
+		{
+//		System.out.println(str);//原样输出读到的内容，此处即可根据规则进行内容切分
+		String[] sss=str.split("\t");
+		for(String sssss:sss) {
+			System.out.println(sssss);
+		}
+		};
+		}catch(Exception e) {
+			
 		}
 	}
 }
